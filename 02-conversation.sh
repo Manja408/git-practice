4bea785c99c916d2a76ab4f3a5cd90e7d4e7323a
Manja408 <manjunatha.blr22>
2026-02-07 00:31:26 +0530

    added new conversation file

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh ::Hi Suresh ,How are you?"
+echo "Suresh ::Hello Ramesh ,i am fine. How are you doing?"
+echo "Ramesh :: I am doing good Suresh.what's going on?"
+echo "Suresh :: I started learning Shell Script Ramesh"
