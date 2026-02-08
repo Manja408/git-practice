d8228bba4fafe7c7f1204facad6d7e75552cdc1f
Manja408 <manjunatha.blr22>
2026-02-09 01:04:03 +0530

    functions

---
+++ 08-functions.sh
@@ -0,0 +1,40 @@
+!#/bin/bash
+
+USERID= $(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0]
+    then 
+        echo "$2 is ...FAILED"
+    else
+        echo "$3 is ...success"
+    fi
+}
+
+if [$USERID -ne 0 ]
+then    
+    echo "please run this script with preveleges"
+    exit 1
+fi
+
+dnf list installed git
+
+if($? -ne 0)
+then
+    echo "MYSQL is not installed....going to install"
+    dnf install git -y
+    VALIDATE $? "installing Mysql"
+else
+    echo"Already isntalled,nothing to do"
+fi
+
+dnf list installed mysql
+
+if($? -ne 0)
+then
+    echo "MYSQL is not installed....going to install"
+    dnf install mysql -y
+    VALIDATE $? "installing Mysql"
+else
+    echo"Already isntalled,nothing to do"
+fi
