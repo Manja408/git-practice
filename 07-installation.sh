63fffc801a99f4ea09f15c3b842de6e69e671d9c
Manja408 <manjunatha.blr22>
2026-02-09 00:12:04 +0530

    functions

--- 07-installation.sh
+++ 07-installation.sh
@@ -3,9 +3,18 @@
 USERID=$(id -u)
 # echo "User ID is: $USERID"
 
-if [ $USERID -ne 0 ]
+if  [ $USERID -ne 0 ]
 then
     echo "Please run this script with root privaleges"
+    exit 1
 fi
 
-dnf install mysql
+dnf install mysqld
+
+if($? -ne 0)
+then
+    echo "MYSQL is not installed"
+    dnf install mysqld
+else
+    echo"Already isntalled,nothing to do"
+fi
