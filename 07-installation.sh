9d8e72e3eb0be0185edbc03cc2849b83969585a3
Manja408 <manjunatha.blr22>
2026-02-09 00:19:46 +0530

    functions

--- 07-installation.sh
+++ 07-installation.sh
@@ -15,6 +15,13 @@ if($? -ne 0)
 then
     echo "MYSQL is not installed"
     dnf install mysql -y
+    if [$? -ne 0 ]
+    then
+        echo "Mysql installation is failure...please check"
+        exit 1
+    else
+        echo "Mysql installation is success"  
+    fi  
 else
     echo"Already isntalled,nothing to do"
 fi
