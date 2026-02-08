c33a33e25f802111597cfcbabdc8b2c532b158e5
Manja408 <manjunatha.blr22>
2026-02-08 12:55:22 +0530

    functions

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -3,4 +3,4 @@
 echo "All variables passed to the script : $@"
 echo "number of variables as passed :$#"
 echo "Script name: $0"
-echo "Current working directory :$pwd"
+echo "Current working directory :$PWD"
