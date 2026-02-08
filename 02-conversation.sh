4a201095c588171ee0c081c33b1806a5ed995994
Manja408 <manjunatha.blr22>
2026-02-08 12:21:18 +0530

    functions

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -5,6 +5,6 @@ echo "Ramesh ::Hi Suresh ,How are you?"
 echo "Suresh ::Hello Ramesh ,i am fine. How are you doing?"
 echo "Ramesh :: I am doing good Suresh.what's going on?"
 echo "Suresh :: I started learning Shell Script Ramesh"
-echo "Manja"
+echo "Manja" 
 
 
